6c5ccb558e271ee59a92c4bbb953f76318d84977
matheuscin <mbcv@cin.ufpe.br>
2024-07-31 23:01:20 -0300

    Update bolinhas_pequenas.py

--- bolinhas_pequenas.py
+++ bolinhas_pequenas.py
@@ -6,12 +6,12 @@ import math
 from classe_italo import ItaloSena
 
 class Bolinha_pequena(ItaloSena):        
-    def __init__(self, x, y, bolinhas_grandes, mapa, posicao_inicial):
-        super().__init__(posicao_inicial)  # Inicializa ItaloSena
+    def __init__(self, x, y, bolinhas_grandes, mapa):
         self.posicoes = []
         self.mapa = mapa
         self.gerar_posicoes(x, y, bolinhas_grandes)
-    
+        self.qtd_bolinhas = 0
+
     def gerar_posicoes(self, x_inicial, y_inicial, bolinhas_grandes):
         def posicao_valida(x, y):
             if 0 <= y // 47 < len(self.mapa) and 0 <= x // 47 < len(self.mapa[0]):
@@ -29,12 +29,14 @@ class Bolinha_pequena(ItaloSena):
         for pos in self.posicoes:
             pygame.draw.circle(screen, (255, 255, 0), pos, constantes.RAIO_BOLINHA_PEQUENA)
     
-    def verificar_colisao(self):
+    def verificar_colisao(self, position):
         to_remove = []
         for pos in self.posicoes:
-            distancia = math.sqrt((pos[0] - self.posicao.x) ** 2 + (pos[1] - self.posicao.y) ** 2)
+            distancia = math.sqrt((pos[0] - position.x) ** 2 + (pos[1] - position.y) ** 2)
             if distancia < constantes.RAIO_BOLINHA_PEQUENA + 19:  # Considerando que 19 é o raio do personagem
                 to_remove.append(pos)
         
         for pos in to_remove:
             self.posicoes.remove(pos)
+            self.qtd_bolinhas += 1
+        
