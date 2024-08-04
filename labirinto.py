a5ae8f09e278b4f2e59962737b95dc2a5d03d9a6
Marina Rodas <122946394+marsales@users.noreply.github.com>
2024-08-04 17:16:54 -0300

    Update labirinto.py

--- labirinto.py
+++ labirinto.py
@@ -1,14 +1,17 @@
 import pygame
-import classe_italo
 from pathlib import Path
+
+# Esse documento contém o labirinto utilizado no nosso jogo e a função para checar a colisão com ele
 
 labirinto_imagem = pygame.image.load(Path('imgs', 'paredes.png'))
 labirinto_rect = labirinto_imagem.get_rect()
 labirinto_mask = pygame.mask.from_surface(labirinto_imagem)
 
+# ----------------------------------- CHECAR COLISÃO -------------------------------------
 def checar_colisao(jogador):
     jogador_mask = jogador.get_mask()
     offset = (jogador.hit_box.x - labirinto_rect.x, jogador.hit_box.y - labirinto_rect.y)
     if labirinto_mask.overlap(jogador_mask, offset):
         return True
     return False
+# ----------------------------------------------------------------------------------------
